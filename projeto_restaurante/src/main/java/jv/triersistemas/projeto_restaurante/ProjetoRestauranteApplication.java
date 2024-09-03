ffa71df53c316b5d75c34e424f5be79528b0a1af
Tarcísio Luiz Pereira Bogo <131721143+tk4500@users.noreply.github.com>
2024-09-03 14:52:13 -0300

    a

---
+++ projeto_restaurante/src/main/java/jv/triersistemas/projeto_restaurante/ProjetoRestauranteApplication.java
@@ -0,0 +1,13 @@
+package jv.triersistemas.projeto_restaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoRestauranteApplication.class, args);
+	}
+
+}
---
+++ projeto_restaurante/src/test/java/jv/triersistemas/projeto_restaurante/ProjetoRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package jv.triersistemas.projeto_restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
